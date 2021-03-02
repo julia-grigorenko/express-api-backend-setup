34d997a75fa1ab1575dd9025a860cc6e1e28cbc0
Julia Grigorenko <jvgrigorenko@gmail.com>
2021-03-02 20:02:52 +0300

    implement postgres connection

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import { indexPage } from '../controllers';
+import { indexPage, messagesPage } from '../controllers';
+
 const indexRouter = express.Router();
 
+console.log(messagesPage);
 indexRouter.get('/', indexPage);
-
+indexRouter.get('/messages', messagesPage);
 export default indexRouter;
