01798c206cd7bc7021973b767eb5bfec78896d8a
Julia Grigorenko <jvgrigorenko@gmail.com>
2021-03-02 20:28:48 +0300

    add functionality to add messages to db

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { indexPage, messagesPage } from '../controllers';
+import { indexPage, messagesPage, addMessage } from '../controllers';
 
 const indexRouter = express.Router();
 
-console.log(messagesPage);
 indexRouter.get('/', indexPage);
 indexRouter.get('/messages', messagesPage);
+indexRouter.post('/messages', addMessage);
 export default indexRouter;
